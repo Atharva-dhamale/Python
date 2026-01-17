6b1cb1c7b1acf63cba7d741d70be3643d4d54ba5
Atharva Kailash Dhamale <atharvadhamale43@gmail.com>
2026-01-17 14:12:10 +0530

    Add files via upload

---
+++ Classwork/RevisionAll.py
@@ -0,0 +1,16 @@
+
+
+print("-"*25)
+
+Arr={10,20,30,40,10}
+print(Arr)
+print(type(Arr))
+
+print("-"*25)
+
+Book={"Name":"Let us C","Price":350,"Author":"Y Kanitkar"}
+print(Book)
+print(type(Book))
+print(Book["Price"])
+
+print("-"*25)
---
+++ Classwork/RevisionSequence.py
@@ -0,0 +1,13 @@
+
+#Sequence Datatypes
+
+print("-"*25)
+Arr=[10,20,30,40,10]
+print(Arr)
+print(type(Arr))
+print("-"*25)
+
+Brr=(10,20,30,40,10)
+print(Brr)
+print(type(Brr))
+print("-"*25)
