ea7f36b2f9b9df0e0b213190790e09f8906c87ae
Atharva Kailash Dhamale <atharvadhamale43@gmail.com>
2026-02-11 23:17:06 +0530

    Add files via upload

---
+++ Classwork/LocalGlobal1.py
@@ -0,0 +1,10 @@
+
+
+No=11                   #Global
+
+def fun():
+    No=21               #Local
+    print("Value of No from Fun is :",No)           #21
+
+print("Value of No is :",No)                        #11
+fun()
