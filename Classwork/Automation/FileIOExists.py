f29c7063ee390716c529ea9fe65d1d46a49f65e3
Atharva Kailash Dhamale <atharvadhamale43@gmail.com>
2026-04-30 19:16:30 +0530

    Add files via upload

---
+++ Classwork/Automation/FileIOExists.py
@@ -0,0 +1,20 @@
+
+import os
+
+def main():
+
+    FileName=input("Enter name of file :")
+
+    Ret=os.path.exists(FileName)
+
+    if(Ret==True):
+
+        fobj=open(FileName,"r")
+        print("File gets succesfully opened")
+
+    else:
+        print("There is no such file")
+
+
+if __name__=="__main__":
+    main()
